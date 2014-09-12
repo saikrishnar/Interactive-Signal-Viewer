d23ba70cfe1dd7794fd1809ab6efdf76aaf8fcf2
Sai Krishna <saikrishna.r@research.iiit.ac.in>
2014-09-12 20:35:45 +0530

    Initial commit

---
+++ controllers/default.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+import csv
+import os
+fp = os.path.join(request.folder,'private','test.csv')
+data1 = []
+data2 = []
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Welcome to Sleep Pattern Analyser!")
+    return dict()
+
+def login():
+ 
+     return dict(form=auth.login())
+    
+
+    
+    
+
+
+def profile():
+
+    with open(fp, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+                data1.append(float((row[0])))
+                data2.append(float((row[1])))
+
+
+    print data1
+    return dict(data1=data1[0:1500],data2 = data2[0:1500])
